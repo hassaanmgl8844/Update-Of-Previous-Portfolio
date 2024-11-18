c568527f44772fa09f7c9042f32119edeeeb9e59
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 02:02:53 +0500

    Added a Font in the Navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,17 +1,29 @@
-
 const Header = () => {
-    return (
-        <nav className="flex  h-[12vh] px-10 justify-between bg-bgColor items-center">
-            <h1 className="text-primaryColor">Hassaam</h1>
-            <div className="flex gap-4">
-                <a className="text-textColor hover:text-primaryColor" href="#about">About</a>
-                <a className="text-textColor hover:text-primaryColor" href="#work">Work</a>
-                <a className="text-textColor hover:text-primaryColor" href="#experience">Experience</a>
-                <a className="text-textColor hover:text-primaryColor" href="#skills">Skills</a>
-                <a className="text-textColor hover:text-primaryColor" href="#contact">Contact</a>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex  h-[12vh] px-10 dm-mono-regular justify-between bg-bgColor items-center">
+      <h1 className="text-primaryColor">Hassaam</h1>
+      <div className="flex gap-4">
+        <a className="text-textColor hover:text-primaryColor" href="#about">
+          About
+        </a>
+        <a className="text-textColor hover:text-primaryColor" href="#work">
+          Work
+        </a>
+        <a
+          className="text-textColor hover:text-primaryColor"
+          href="#experience"
+        >
+          Experience
+        </a>
+        <a className="text-textColor hover:text-primaryColor" href="#skills">
+          Skills
+        </a>
+        <a className="text-textColor hover:text-primaryColor" href="#contact">
+          Contact
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
