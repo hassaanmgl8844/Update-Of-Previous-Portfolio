9bec01b720d4f83b0d083833da1083e46a378548
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 00:41:26 +0500

    HomePage Is Setup Now We Are Ready to build the Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
-    <h1 className='text-red-500'>Hello World</h1>
+    <HomePage />
   );
 }
 
