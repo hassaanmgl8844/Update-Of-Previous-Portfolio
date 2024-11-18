2a56de2ee13babf10db41087c5a37630be045524
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 01:04:12 +0500

    Just Finish the HTML Stuff of Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,7 +5,11 @@ const Header = () => {
         <nav>
             <h1>Hassaam</h1>
             <div>
-                <Anchor href="#about" target="_blank">About</Anchor>
+                <Anchor href="#about" target="_blank" underline="never">About</Anchor>
+                <Anchor href="#work" target="_blank" underline="never">Work</Anchor>
+                <Anchor href="#experience" target="_blank" underline="never">Experience</Anchor>
+                <Anchor href="#skills" target="_blank" underline="never">Skills</Anchor>
+                <Anchor href="#contact" target="_blank" underline="never">Contact</Anchor>
             </div>
         </nav>
     )
