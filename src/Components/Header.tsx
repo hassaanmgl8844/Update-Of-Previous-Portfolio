063eba209dad2dcaa7d7c6c70fc0e3be81b532db
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 01:44:02 +0500

    Added Padding in the Navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,10 @@
 
 const Header = () => {
     return (
-        <nav className="flex text-primaryColor justify-between bg-bgColor">
+        <nav className="flex text-primaryColor px-4  py-3 justify-between bg-bgColor">
             <h1 className="">Hassaam</h1>
             <div>
-                <a className="text-textColor" href="#about" target="_blank" >About</a>
+                <a className="text-FtextColor" href="#about" target="_blank" >About</a>
                 <a className="text-[#8892B0]" href="#work" target="_blank" >Work</a>
                 <a className="text-[#8892B0]" href="#experience" target="_blank" >Experience</a>
                 <a className="text-[#8892B0]" href="#skills" target="_blank" >Skills</a>
