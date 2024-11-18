6e0f521a46ce6f33edee43b446827e684f8c54fc
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 01:16:01 +0500

    Removed Anchor and Used a in the navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,14 @@
-import { Anchor } from "@mantine/core"
 
 const Header = () => {
     return (
         <nav className="flex text-[#64FFDA] justify-between bg-[#0A192F]">
             <h1 className="">Hassaam</h1>
             <div>
-                <Anchor href="#about" target="_blank" underline="never">About</Anchor>
-                <Anchor href="#work" target="_blank" underline="never">Work</Anchor>
-                <Anchor href="#experience" target="_blank" underline="never">Experience</Anchor>
-                <Anchor href="#skills" target="_blank" underline="never">Skills</Anchor>
-                <Anchor href="#contact" target="_blank" underline="never">Contact</Anchor>
+                <a href="#about" target="_blank" >About</a>
+                <a href="#work" target="_blank" >Work</a>
+                <a href="#experience" target="_blank" >Experience</a>
+                <a href="#skills" target="_blank" >Skills</a>
+                <a href="#contact" target="_blank" >Contact</a>
             </div>
         </nav>
     )
