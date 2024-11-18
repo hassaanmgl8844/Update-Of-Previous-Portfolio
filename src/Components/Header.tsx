d251732a6686319159f501cfdd2f93d71487e0c4
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 01:35:21 +0500

    Have build a  Template of colors to use whereever you want

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,14 @@
 
 const Header = () => {
     return (
-        <nav className="flex text-[#64FFDA] justify-between bg-[#0A192F]">
+        <nav className="flex text-primaryColor justify-between bg-bgColor">
             <h1 className="">Hassaam</h1>
             <div>
-                <a href="#about" target="_blank" >About</a>
-                <a href="#work" target="_blank" >Work</a>
-                <a href="#experience" target="_blank" >Experience</a>
-                <a href="#skills" target="_blank" >Skills</a>
-                <a href="#contact" target="_blank" >Contact</a>
+                <a className="text-textColor" href="#about" target="_blank" >About</a>
+                <a className="text-[#8892B0]" href="#work" target="_blank" >Work</a>
+                <a className="text-[#8892B0]" href="#experience" target="_blank" >Experience</a>
+                <a className="text-[#8892B0]" href="#skills" target="_blank" >Skills</a>
+                <a className="text-[#8892B0]" href="#contact" target="_blank" >Contact</a>
             </div>
         </nav>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      bgColor: "#0A192F",
+      primaryColor: "#64FFDA",
+      textColor: "#8892B0",
+    }
   },
   plugins: [],
 }
