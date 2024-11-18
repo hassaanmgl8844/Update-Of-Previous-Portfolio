da8c6e193a8d8eb4a8b542a94824282775f11358
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 02:24:16 +0500

    99% Work Done of Navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,8 +2,8 @@ import { IconHexagonLetterH } from "@tabler/icons-react";
 
 const Header = () => {
   return (
-    <nav className="flex  h-[12vh] px-10 dm-mono-regular justify-between bg-bgColor items-center">
-        <IconHexagonLetterH size={70} color="#64FFDA" />
+    <nav className="flex  h-[9vh] px-10 dm-mono-regular justify-between bg-bgColor items-center">
+        <IconHexagonLetterH size={54} color="#64FFDA" stroke={1.2} />
       {/* <h1 className="text-primaryColor">Hassaam</h1> */}
       <div className="flex gap-4">
         <a className="text-textColor hover:text-primaryColor" href="#about">
